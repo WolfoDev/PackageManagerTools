d418156ebe4267e7219623721577a1afe7ca59ec
Wolfo <f.greco@hevolus.it>
2019-06-21 15:43:11 +0200

    Renamed package name to package tools

--- Scripts/Editor/PackageUpdater.cs
+++ Scripts/Editor/PackageUpdater.cs
@@ -2,25 +2,28 @@
 using UnityEditor;
 using System.IO;
 
-public class PackageUpdater : EditorWindow
+namespace PackageTools
 {
-    [MenuItem("File/Update Packages")]
-    static void UpdatePackages()
-    {
-        if (!EditorUtility.DisplayDialog("WARNING", $"You are about to update ALL packages.", "Okay", "Cancel"))
-            return;
+	public class PackageUpdater : EditorWindow
+	{
+		[MenuItem("File/Update Packages")]
+		static void UpdatePackages()
+		{
+			if (!EditorUtility.DisplayDialog("WARNING", $"You are about to update ALL packages.", "Okay", "Cancel"))
+				return;
 
-        DirectoryInfo pathToManifest = new DirectoryInfo($"{Application.dataPath}/../Packages/manifest.json");
-        string manifestData = File.ReadAllText(pathToManifest.FullName);
-        Debug.Log(manifestData.Contains("\"lock\""));
-        manifestData = manifestData.Replace("\"lock\"", "\"trash\"");//TODO understand why
-        Debug.Log(manifestData);
-        File.WriteAllText(pathToManifest.FullName, manifestData);
-    }
+			DirectoryInfo pathToManifest = new DirectoryInfo($"{Application.dataPath}/../Packages/manifest.json");
+			string manifestData = File.ReadAllText(pathToManifest.FullName);
+			Debug.Log(manifestData.Contains("\"lock\""));
+			manifestData = manifestData.Replace("\"lock\"", "\"trash\"");//TODO understand why
+			Debug.Log(manifestData);
+			File.WriteAllText(pathToManifest.FullName, manifestData);
+		}
 
-    [MenuItem("File/Update Packages", true)]
-    static bool UpdatePackagesCheck(MenuCommand menuCommand)
-    {
-        return File.Exists($"{Application.dataPath}/../Packages/manifest.json");
-    }
+		[MenuItem("File/Update Packages", true)]
+		static bool UpdatePackagesCheck(MenuCommand menuCommand)
+		{
+			return File.Exists($"{Application.dataPath}/../Packages/manifest.json");
+		}
+	}
 }
